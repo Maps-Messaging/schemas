33d2adec15203f76393dbe42d639d98b089d901f
Matthew Buckton <matthew@buckton.org>
2022-07-05 13:12:03 +1000

    clean up sonar lint issues

---
+++ src/test/java/io/mapsmessaging/schemas/formatters/TestFormatConstructors.java
@@ -0,0 +1,53 @@
+package io.mapsmessaging.schemas.formatters;
+
+import io.mapsmessaging.schemas.config.SchemaConfig;
+import io.mapsmessaging.schemas.config.impl.RawSchemaConfig;
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TestFormatConstructors {
+
+  @Test
+  void validSchemaLoad() throws IOException {
+    SchemaConfig good = new RawSchemaConfig();
+    good.setUniqueId(UUID.randomUUID());
+    Assertions.assertNotNull(MessageFormatterFactory.getInstance().getFormatter(good));
+  }
+
+
+  @Test
+  void invalidSchemaLoad() {
+    SchemaConfig bad = new BadSchema();
+    bad.setUniqueId(UUID.randomUUID());
+    Assertions.assertThrowsExactly(IOException.class, () -> MessageFormatterFactory.getInstance().getFormatter(bad));
+  }
+
+  static class BadSchema extends SchemaConfig {
+
+    protected BadSchema() {
+      super("BAD");
+      uniqueId = UUID.randomUUID();
+    }
+
+    protected BadSchema(String format, Map<String, Object> config) {
+      super(format, config);
+    }
+
+    @Override
+    protected JSONObject packData() throws IOException {
+      JSONObject data = new JSONObject();
+      packData(data);
+      return data;
+    }
+
+    @Override
+    protected SchemaConfig getInstance(Map<String, Object> config) {
+      return this;
+    }
+  }
+
+}

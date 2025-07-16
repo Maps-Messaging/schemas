68ef94f296f2a35cd2fb73cd27b44a5fd8440691
Matthew Buckton <matthew@buckton.org>
2025-07-16 19:24:27 +1000

    bit more debug

--- src/test/java/io/mapsmessaging/schemas/formatters/BaseTest.java
+++ src/test/java/io/mapsmessaging/schemas/formatters/BaseTest.java
@@ -1,21 +1,18 @@
 /*
  *
- *  Copyright [ 2020 - 2024 ] Matthew Buckton
- *  Copyright [ 2024 - 2025 ] MapsMessaging B.V.
+ *     Copyright [ 2020 - 2025 ] [Matthew Buckton]
  *
- *  Licensed under the Apache License, Version 2.0 with the Commons Clause
- *  (the "License"); you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at:
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *      https://commonsclause.com/
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
  */
 
 package io.mapsmessaging.schemas.formatters;
@@ -203,7 +200,7 @@ public abstract class BaseTest {
     MessageFormatter formatter = MessageFormatterFactory.getInstance().getFormatter(schemaConfig);
     List<DataSet> result = dataSet.parallelStream().filter(dataSet1 -> executor.evaluate(formatter.parse(dataSet1.packed))).collect(Collectors.toList());
 
-    Assertions.assertTrue(result.size() >= 5);
+    Assertions.assertTrue(result.size() >= 5, "result.size() "+result.size()+" >= 5");
     long time = (System.currentTimeMillis() - start);
     float unitWork = time;
     unitWork = unitWork / data.size();
